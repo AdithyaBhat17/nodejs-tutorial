c1ee234c552346110f4c7dd194d511e633821693
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2020-01-27 00:21:30 +0530

    :sparkles: removed console logs

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const url = require('url');
 const fs = require('fs');
 
 let server = http.createServer(function (req, res) {
-    console.log(url.parse(req.url, true).pathname, req.method)
     if(url.parse(req.url, true).pathname === '/hello' && req.method === 'GET') {
         res.writeHead(200, 'Success');
         fs.readFile('./index.html', function (error, result) {
@@ -12,9 +11,8 @@ let server = http.createServer(function (req, res) {
             res.write(result);
             res.end();
         });
-        // res.end();
+        // res.end(); to show non-blocking I/O
     }
-
 });
 
 server.listen(8080, 'Localhost', () => console.log('up and running'));
